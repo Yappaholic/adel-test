23542da9f83cd9e6397ef1ed231a206e9809f717
Yappaholic <sav.boyar@gmail.com>
2025-02-26 16:14:00 +0300

    Feat: update next config for image loading

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "**",
+        search: "",
+      },
+    ],
+  },
   /* config options here */
 };
 
