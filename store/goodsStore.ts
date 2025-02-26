152e01b633a5b7a4587819cff861cb7c6707823c
Yappaholic <sav.boyar@gmail.com>
2025-02-26 16:14:28 +0300

    Feat: update store to preload goods as module

--- store/goodsStore.ts
+++ store/goodsStore.ts
@@ -1,21 +1,23 @@
-import { Product } from "@/types/data";
+import { IProduct } from "@/types/data";
 import { create } from "zustand";
 import type { TSort } from "@/types/sort";
 interface IGoodsStore {
-  goods: Array<Product>;
+  goods: Array<IProduct>;
   sorting: TSort;
   updateSorting: (sortingType: TSort) => void;
-  fetchGoods: (url: string) => void;
+  fetch: (url: string) => void;
 }
 
 const useStore = create<IGoodsStore>((set) => ({
   goods: [],
   sorting: "По релевантности",
   updateSorting: (sortingType: TSort) => set({ sorting: sortingType }),
-  fetchGoods: async (url: string) => {
+  fetch: async (url: string) => {
     const response = await fetch(url);
     set({ goods: await response.json() });
   },
 }));
+
+useStore.getState().fetch("/api/products");
 
 export default useStore;
