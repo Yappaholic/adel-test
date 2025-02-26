b534876c5df14738881544e488f8102e9bf4d5a0
Yappaholic <sav.boyar@gmail.com>
2025-02-26 12:53:57 +0300

    Feat: add container

---
+++ components/Container.tsx
@@ -0,0 +1,14 @@
+import clsx from "clsx/lite";
+interface Props {
+  className: string;
+}
+const Container: React.FC<React.PropsWithChildren<Props>> = ({
+  className,
+  children,
+}) => {
+  return (
+    <div className={clsx("mx-auto max-w-[1420]", className)}>{children}</div>
+  );
+};
+
+export default Container;
